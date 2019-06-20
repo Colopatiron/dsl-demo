85adf5f4650bc1306114a0f3be2ada7c953349ea
Jem Pimentel <jem.pimentel@outlook.com>
2019-06-20 15:24:15 +0800

    Adds dsl demo code

--- lib/dsl_demo.rb
+++ lib/dsl_demo.rb
@@ -1,6 +1,42 @@
 require "dsl_demo/version"
 
 module DslDemo
-  class Error < StandardError; end
-  # Your code goes here...
+  class Recipe
+    @recipes = Hash.new do |hash, key|
+      hash[key] = { ingredients: [], instructions: [] }
+    end
+
+    class << self
+      def for(recipe_name)
+        <<~RECIPE if @recipes.has_key?(recipe_name)
+          #{recipe_name}
+
+          Ingredients:
+          #{@recipes[recipe_name][:ingredients].uniq * "\n"}
+
+          Instructions:
+          #{@recipes[recipe_name][:instructions].uniq * "\n"}
+        RECIPE
+      end
+
+      def describe(&blk)
+        instance_eval(&blk) if block_given?
+      end
+
+      private
+
+        def ingredient(ingredient_name)
+          @recipes[@recipe_name][:ingredients] << ingredient_name
+        end
+
+        def instruction(instruction_name)
+          @recipes[@recipe_name][:instructions] << instruction_name
+        end
+
+        def recipe(recipe_name)
+          @recipe_name = recipe_name
+          yield if block_given?
+        end
+    end
+  end
 end
